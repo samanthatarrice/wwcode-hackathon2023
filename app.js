8f12ff14b71bb907e8de0fc7fda40dd6350b2771
Aisha Umar <igotwebmail@gmail.com>
2023-10-22 19:28:59 -0400

    added about path

--- app.js
+++ app.js
@@ -32,6 +32,10 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
 app.listen(3000, () => {
   console.log("Serving Lavender on port 3000");
 });
